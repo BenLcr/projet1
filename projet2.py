4608ea52f07d3058cb6e29beada3189887a48a5d
BenLcr <104418214+BenLcr@users.noreply.github.com>
2024-05-14 15:35:42 +0200

    Update and rename projet2 to projet2.py

--- projet2.py
+++ projet2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from ucimlrepo import fetch_ucirepo 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
 """iris = load_iris()
 X, Y = iris.data, iris.target
 
@@ -16,31 +16,35 @@ clf = clf.fit(X, Y)
 plt.figure(figsize=(12, 10))
 tree.plot_tree(clf)"""
   
-# fetch dataset 
-heart_disease = fetch_ucirepo(id=45) 
-  
-# data (as pandas dataframes) 
-X = heart_disease.data.features 
-y = heart_disease.data.targets 
-  
-# variable information 
-print(heart_disease.variables) 
 
 Xdf = pd.read_csv('dataset.csv')
 y = Xdf['target']
 
 Xdf = Xdf.dropna()
 
+Xdf.target.value_counts()
+sns.countplot(x='target', data=Xdf)
+
+print("Nombre de personnes malades :", len(Xdf[Xdf.target == 0]))
+print("Nombre de personnes non malades :", len(Xdf[Xdf.target == 1]))
+
 #%% Analyse univariée des variables
-Xdf.boxplot(by='target')
-Xdf.groupby('target').mean()
+# Xdf.boxplot(by='target')
+# Xdf.groupby('target').mean()
 
 #%% Analyse bivariée des variables
-pd.plotting.scatter_matrix(Xdf.iloc[:,:13])
+correlation_matrix = Xdf.corr()
+
+# Heatmap avec seaborn
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=True, vmin=-1, vmax=1, center=0, cmap='coolwarm')
+plt.tight_layout()
+plt.title("Matrice de corrélation")
+plt.show()
 
 #%% Analyse multivariée 
 adl = LinearDiscriminantAnalysis()
-Xadl = adl.fit_transform(Xdf.iloc[:,:13], Xdf['target'])
+Xadl = adl.fit_transform(Xdf.iloc[:,:12], Xdf['target'])
 
 #%%  
 plt.bar(np.arange(1, len(adl.explained_variance_ratio_)+1), adl.explained_variance_ratio_)
@@ -50,17 +54,17 @@ plt.xlabel("Nombre de facteurs")
 plt.show()
 
 #%% 
-scatter = plt.scatter(Xadl[:, 0], Xadl[:, 1], c=Xdf['target'])
+rand = np.random.randn(int(np.shape(Xadl)[0]))
+scatter = plt.scatter(Xadl[:, 0], rand, c=Xdf['target'])
 plt.xlabel('Composante discriminante 1')
 plt.ylabel('Composante discriminante 2')
-plt.title('LDA of IRIS dataset')
+plt.title('LDA of Hearth diseases dataset')
 plt.legend(*scatter.legend_elements())
 plt.show()
 
 corvar = np.zeros((Xdf.shape[1] - 1, 2))
 for i in range(Xdf.shape[1] - 1):
     corvar[i, 0] = np.corrcoef(Xadl[:, 0], Xdf.iloc[:, i])[0, 1]
-    corvar[i, 1] = np.corrcoef(Xadl[:, 1], Xdf.iloc[:, i])[1, 0]
 
 fig, axes = plt.subplots(figsize=(8, 8))
 axes.set_xlim(-1, 1)
@@ -79,8 +83,8 @@ plt.plot([0, 0], [-1, 1], color='silver', linestyle='-', linewidth=1)
 cercle = plt.Circle((0, 0), 1, color='blue', fill=False)
 axes.add_artist(cercle)
 
-x_train, x_test, y_train, y_test = train_test_split(Xdf.iloc[:, :13], Xdf['target'], random_state=0)
-clf = tree.DecisionTreeClassifier(max_depth=4)
+x_train, x_test, y_train, y_test = train_test_split(Xdf.iloc[:, :12], Xdf['target'], test_size=0.2, random_state=0)
+clf = tree.DecisionTreeClassifier()
 clf = clf.fit(Xdf, Xdf['target'])
 
 y_pred = clf.predict(x_test)
