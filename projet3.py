02c669dae7afe056373896d6108214b0595df63c
BenLcr <104418214+BenLcr@users.noreply.github.com>
2024-05-27 12:26:00 +0200

    Ajout graph grid search

--- projet3.py
+++ projet3.py
@@ -11,6 +11,31 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.exceptions import ConvergenceWarning
+import warnings as w
+w.simplefilter("ignore", category=ConvergenceWarning)
+
+def plot_grid_search(cv_results, grid_param_1, grid_param_2, name_param_1, name_param_2):
+    # Get Test Scores Mean and std for each grid search
+    scores_mean = cv_results['mean_test_score']
+    scores_mean = np.array(scores_mean).reshape(len(grid_param_2),len(grid_param_1))
+
+    scores_sd = cv_results['std_test_score']
+    scores_sd = np.array(scores_sd).reshape(len(grid_param_2),len(grid_param_1))
+
+    # Plot Grid search scores
+    _, ax = plt.subplots(1,1)
+
+    # Param1 is the X-axis, Param 2 is represented as a different curve (color line)
+    for idx, val in enumerate(grid_param_2):
+        ax.plot(grid_param_1, scores_mean[idx,:], '-o', label= name_param_2 + ': ' + str(val))
+
+    plt.tight_layout()
+    ax.set_title("Grid Search Scores", fontsize=20, fontweight='bold')
+    ax.set_xlabel(name_param_1, fontsize=16)
+    ax.set_ylabel('CV Average Score', fontsize=16)
+    ax.legend(loc="best", fontsize=15)
+    ax.grid('on')
 
 def accuracy_cm(confusion_matrix):
     correct_predictions = np.diag(confusion_matrix).sum()
@@ -18,37 +43,29 @@ def accuracy_cm(confusion_matrix):
     accuracy = correct_predictions / total_predictions
     return accuracy
 
-# Chargement et préparation des données
-Xdf = pd.read_csv('database.csv')
+# Chargement et prÃ©paration des donnÃ©es
+Xdf = pd.read_csv('C:/Users/lacou/Desktop/traitement donnÃ©e/Seance-6-SVM-et-MLP/database.csv')
 missing_values = Xdf.isnull().sum()
-print(f"Nombre de données manquantes : {missing_values.sum()}")
+print(f"Nombre de donnÃ©es manquantes : {missing_values.sum()}")
 
 Xdf = Xdf.dropna()
 
 x = Xdf.iloc[:, 3:]
 y = Xdf['Classe']
 
-# Conversion des données en 0 naturel 1 artificiel
+# Conversion des donnÃ©es en 0 naturel 1 artificiel
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 y = 1 - y
 
-# Analyse données
-# Visualisation de la répartition des classes
+# Analyse donnÃ©es
+# Visualisation de la rÃ©partition des classes
 sns.countplot(x=y, data=Xdf)
-plt.title("Répartition des classes dans le dataset")
+plt.title("RÃ©partition des classes dans le dataset")
 plt.show()
 
 print("Nombre de paysages naturels :", (y == 0).sum())
 print("Nombre de paysages artificiels :", (y == 1).sum())
-
-# Affichage de la matrice de corrélation
-correlation_matrix = Xdf.corr()
-plt.figure(figsize=(12, 10))
-sns.heatmap(correlation_matrix, annot=True, vmin=-1, vmax=1, center=0, cmap='coolwarm')
-plt.title("Matrice de corrélation")
-plt.tight_layout()
-plt.show()
 
 # LDA
 adl = LinearDiscriminantAnalysis()
@@ -60,40 +77,57 @@ plt.title("ADL")
 plt.xlabel("Composante discriminante")
 plt.show()
 
-# Séparation des données
+# SÃ©paration des donnÃ©es
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0, stratify=y)
 x_train2, x_validation, y_train2, y_validation = train_test_split(x_train, y_train, test_size=0.33, random_state=0, stratify=y_train)
 
-# Prédiction avec LDA
+# PrÃ©diction avec LDA
 yadl_pred = adl.predict(x_test)
 
 # Affichage de la matrice de confusion pour LDA
-CM_ADL = confusion_matrix(y_test, yadl_pred)
+CM_ADL = confusion_matrix(y_test, yadl_pred, normalize='all')
 disp = ConfusionMatrixDisplay(CM_ADL)
 disp.plot(cmap='hot')
-plt.title("Matrice de confusion - Analyse Discriminante Linéaire")
+plt.title("Matrice de confusion - Analyse Discriminante LinÃ©aire")
 plt.show()
 print("accuracy CM_ADL", accuracy_cm(CM_ADL))
 
 # Training MLP
-tab_accuracy =  []
-tab_conf = []
-nb_neurones = np.linspace(1, 21, 1)
-alpha = np.linspace(0.00001, 0.001, 0.00001)
+#nb_neurones = [n for n in range(1, 21, 2)]
+#print(nb_neurones)
+#alpha = np.linspace(0.00001, 0.01, 10)
+#tab_accuracy = np.zeros((len(alpha), len(nb_neurones)))
+#print(tab_accuracy)
+#
+##for i in range(0, len(alpha) - 1):
+##    for j in range(0, len(nb_neurones) - 1):
+##        clf_MLP = MLPClassifier(random_state=0, hidden_layer_sizes=(int(nb_neurones[i]),), alpha=alpha[j], max_iter=300).fit(x_train2, y_train2)
+##        tab_accuracy[i][j] = clf_MLP.score(x_validation, y_validation)
+#
+## On va faire intervalle de confiance = moyenne des accuracy - val max/val min 
+#
+clf_MLP = MLPClassifier(random_state=0, max_iter=300)
 
-for i in range(0, max(alpha, len(nb_neurones))):
-    clf_MLP = MLPClassifier(random_state=0, hidden_layer_sizes=(nb_neurones,), alpha=alpha, max_iter=300).fit(x_train2, y_train2)
-    y_pred = clf_MLP.predict(x_validation)
-    tab_accuracy.append(clf_MLP.score(x_test, y_test))
-    tab_conf.append()
+# DÃ©finir la grille des hyperparamÃ¨tres Ã  tester
+param_grid = {
+    'hidden_layer_sizes': [(n,) for n in range(1, 14, 1)],  # De 1 Ã  21 neurones dans une seule couche
+    'alpha': np.linspace(0.00001, 0.001, 10)  # 100 valeurs de alpha entre 0.00001 et 0.001
+}
 
-clf_MLP = MLPClassifier(random_state=0, hidden_layer_sizes=(2,), alpha=0.0001, max_iter=300).fit(x_train, y_train)
+grid_search = GridSearchCV(estimator=clf_MLP, param_grid=param_grid, cv=5, scoring='accuracy')
+grid_search.fit(x_train2, y_train2)
 
-# Prédiction avec l'arbre de décision
+# Afficher les meilleurs paramÃ¨tres
+print(f"Meilleurs paramÃ¨tres: {grid_search.best_params_}")
+plot_grid_search(grid_search.cv_results_, grid_search.best_params_['alpha'], grid_search.best_params_["hidden_layer_sizes"], 'alpha', 'hidden_layer_sizes')
+
+clf_MLP = MLPClassifier(random_state=0, hidden_layer_sizes=(11,), alpha=0.00012, max_iter=300).fit(x_train, y_train)
+
+# PrÃ©diction avec MLP
 y_pred = clf_MLP.predict(x_test)
 
 # Affichage de la matrice de confusion pour MLP
-CM_MLP = confusion_matrix(y_test, y_pred)
+CM_MLP = confusion_matrix(y_test, y_pred, normalize='all')
 disp = ConfusionMatrixDisplay(CM_MLP)
 disp.plot(cmap='hot')
 plt.title("Matrice de confusion - MLP")
@@ -102,16 +136,31 @@ print("Score MLP", clf_MLP.score(x_test, y_test))
 print("accuracy CM_CLF", accuracy_cm(CM_MLP))
 
 # Training SVM
+clf_SVM = make_pipeline(StandardScaler(), SVC())
+
+# DÃ©finir la grille des hyperparamÃ¨tres Ã  tester
+param_grid = {
+    'svc__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],  # DiffÃ©rents noyaux Ã  tester
+    'svc__C': np.linspace(0.001, 10.0, 10)  # DiffÃ©rentes valeurs de C Ã  tester
+}
+
+grid_search = GridSearchCV(estimator=clf_SVM, param_grid=param_grid, cv=5, scoring='accuracy')
+grid_search.fit(x_train2, y_train2)
+
+# Afficher les meilleurs paramÃ¨tres
+print(f"Meilleurs paramÃ¨tres: {grid_search.best_params_}")
+plot_grid_search(grid_search.cv_results_, param_grid['svc__C'], param_grid["svc__kernel"], 'C', 'Kernel')
+
 C = 1.0
 kernel = 'linear' #{'linear', 'poly', 'rbf', 'sigmoid'}
 clf_SVM = make_pipeline(StandardScaler(), SVC(C=C, kernel=kernel))
 clf_SVM.fit(x_train, y_train)
 
-# Prédiction avec l'arbre de décision
+# PrÃ©diction avec l'arbre de dÃ©cision
 y_pred = clf_SVM.predict(x_test)
 
 # Affichage de la matrice de confusion pour SVM
-CM_SVM = confusion_matrix(y_test, y_pred)
+CM_SVM = confusion_matrix(y_test, y_pred, normalize='all')
 disp = ConfusionMatrixDisplay(CM_SVM)
 disp.plot(cmap='hot')
 plt.title("Matrice de confusion - SVM")
